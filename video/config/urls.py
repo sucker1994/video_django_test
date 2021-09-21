7c09aa12b429c41b247ced1c654f8b08bba8cca2
sucker1994 <840650089@qq.com>
2021-09-21 21:43:53 +0800

    这是初始化的版本

---
+++ video/config/urls.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from django.contrib import admin
+from django.urls import path, include
+from app.client import urls as client_urls
+from app.dashboard import urls as dashboard_urls
+
+urlpatterns = [
+    path('dashboard/', include(client_urls)),
+    path('client/', include(dashboard_urls))
+]
